1c6ffe7545da6dd6df5e942628d2e09e083a3fe8
doozzi <gustn2607@gmail.com>
2023-04-12 19:24:34 +0900

    drftodo

--- 04_DRFTodo/todo/views.py
+++ 04_DRFTodo/todo/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Todo
+from .serializers import TodoSimpleSerializer
+
+class TodosAPIView(APIView):
+    def get(self, requeset):
+        todos = Todo.objects.filter(complete=False)
+        serializer = TodoSimpleSerializer(todos, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
