7d623ee723f273bce0c628b16c477287e1fb1d7b
doozzi <gustn2607@gmail.com>
2023-04-17 19:04:43 +0900

    myboard

--- 05_MyBoard/users/serializers.py
+++ 05_MyBoard/users/serializers.py
@@ -1,9 +1,14 @@
+from django.contrib.auth import authenticate    
+# Django의 기본 authenticate 함수, 설정한 DefaultAuthBackend인 TokenAuth 방식으로 유저 인증
+
 from django.contrib.auth.models import User     # User Model
 from django.contrib.auth.password_validation import validate_password       # Django 기본 패스워드 검증 도구
 
 from rest_framework import serializers 
 from rest_framework.authtoken.models import Token       # Token Model
 from rest_framework.validators import UniqueValidator   # 이메일 중복 방지를 위한 검증 도구
+
+from .models import Profile
 
 class RegisterSerializer(serializers.ModelSerializer):  # 회원가입 시리얼라이저
     email = serializers.EmailField(
@@ -44,3 +49,22 @@ class RegisterSerializer(serializers.ModelSerializer):  # 회원가입 시리얼
         token = Token.objects.create(user=user)
 
         return user
+    
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+    # write_only 옵션을 통해 클라이언트->서버 방향의 역질렬화 가능, 서버->클라이언트 방향의 직렬화 불가능
+
+    def validate(sefl, data):
+        user = authenticate(**data)
+        if user:
+            token = Token.objects.get(user=user)
+            return token
+        raise serializers.ValidationError(
+            {"error": "Unable to log in with provided credentials"}
+        )
+    
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ("nickname", "position", "subjects", "image")
