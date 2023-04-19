78cc46891723f0ab9f05edb4644f3ee69f3359e9
doozzi <gustn2607@gmail.com>
2023-04-19 12:05:37 +0900

    react-board

---
+++ react-board/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../shared/App.css";
+
+const Header = () => {
+  return (
+    <div className="footer">
+      <p>2023 @Hyunsoo, All rights reserved</p>
+    </div>
+  );
+};
+
+export default Header;
