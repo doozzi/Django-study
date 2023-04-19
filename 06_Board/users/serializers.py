60e8fa8a538e7be35d90a0399de1eb84a7e69781
doozzi <gustn2607@gmail.com>
2023-04-19 10:07:30 +0900

    board

--- 06_Board/users/serializers.py
+++ 06_Board/users/serializers.py
@@ -8,6 +8,8 @@ from rest_framework import serializers
 from rest_framework.authtoken.models import Token       # Token Model
 from rest_framework.validators import UniqueValidator   # 이메일 중복 방지를 위한 검증 도구
 
+from .models import Profile
+
 class RegisterSerializer(serializers.ModelSerializer):  # 회원가입 시리얼라이저
     email = serializers.EmailField(
         required=True,
@@ -47,3 +49,22 @@ class RegisterSerializer(serializers.ModelSerializer):  # 회원가입 시리얼
         token = Token.objects.create(user=user)
 
         return user
+    
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+    # write_only 옵션을 통해 클라이언트->서버 방향의 역질렬화 가능, 서버->클라이언트 방향의 직렬화 불가능
+
+    def validate(sefl, data):
+        user = authenticate(**data)
+        if user:
+            token = Token.objects.get(user=user)
+            return token
+        raise serializers.ValidationError(
+            {"error": "Unable to log in with provided credentials"}
+        )
+    
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ("nickname", "position", "subjects", "image")
